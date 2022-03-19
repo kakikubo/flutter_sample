ab176f54521f9afb3181e59436c76b75ea12b160
kakikubo <kakikubo@gmail.com>
2022-03-19 18:59:39 +0900

    onGenerateRouteを使って値をSecondへ渡す

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       initialRoute: '/',
       routes: {
         '/': (context) => FirstPage(),
-        '/second': (context) => SecondPage(),
       },
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      onGenerateRoute: (settings) {
+        if (settings.name == '/second') {
+          String? messageFromFirst = settings.arguments as String?;
+          return MaterialPageRoute(
+            builder: (context) {
+              return SecondPage(messageFromFirst: messageFromFirst);
+            },
+          );
+        }
+        return null;
+      },
     );
   }
 }
@@ -57,13 +59,13 @@ class FirstPage extends StatelessWidget {
 }
 
 class SecondPage extends StatelessWidget {
-  // final String? messageFromFirst;
-  //
-  // const SecondPage({Key? key, this.messageFromFirst}) : super(key: key);
+  final String? messageFromFirst;
+
+  const SecondPage({Key? key, required this.messageFromFirst})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var messageFromFirst = ModalRoute.of(context)?.settings.arguments;
     print(messageFromFirst);
     return Scaffold(
       appBar: AppBar(title: Text(messageFromFirst as String)),
